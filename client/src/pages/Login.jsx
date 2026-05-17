a016994cc43696e30a4f6c2a1f4a0febcad1b46d
Aniket kumar <aniket2004kr@gmail.com>
2026-05-17 19:34:15 +0530

    undo callback for login and register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -198,7 +198,7 @@ const Login = () => {
 
 
                     <a
-                        href={`${API_BASE_URL}/api/auth/google/callback`}
+                        href={`${API_BASE_URL}/api/auth/google`}
                         className='flex items-center justify-center gap-3
                     w-full border-2 border-indigo-200 rounded-xl
                     py-2.5 text-sm font-semibold text-indigo-700
